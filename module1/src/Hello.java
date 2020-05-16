29636aa3d12f6bf902acddcab672b8baa7b84784
yangfengfan <779520127@qq.com>
2020-05-17 03:44:14 +0800

    yff517wan

---
+++ module1/src/Hello.java
@@ -0,0 +1,22 @@
+
+/**
+ * @author Yangfengfan
+ * @date 2020/05/17
+ */
+public class Hello {
+    public static void main(String[] args) {
+        System.out.println("花式操作1");
+        for (int i = 0; i < 100; i++) {
+            if (i == 18) {
+                System.out.println(i);
+                break;
+            }
+
+            System.out.println("cbhasc");
+            System.out.println("cbhasc");
+            System.out.println("cbhasc");
+        }
+
+
+    }
+}
